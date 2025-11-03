3f45e2bad6a28ff17df9eb80e176236aa3a7b736
Ivan Holm <vinokur4515@gmail.com>
2025-11-04 02:15:34 +0300

    валидация авторизации

--- src/components/common/InputFormRegistr.jsx
+++ src/components/common/InputFormRegistr.jsx
@@ -1,7 +1,7 @@
 const InputFormRegistr = (props) => {
     return (
         <div className="input-field">
-            <label className="block text-left text-m/6 font-medium mb-1 text-gray-100" htmlFor={props.htmlFor}>
+            <label className="block text-left text-2xl/6 font-medium mb-4 text-gray-100" htmlFor={props.htmlFor}>
                 {props.label}
                 {props.required && <span className="required">*</span>}
             </label>
@@ -15,9 +15,12 @@ const InputFormRegistr = (props) => {
                 onChange={props.onChange}
                 autoComplete={props.autoComplete}
                 placeholder={props.placeholder}
-                className={`block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6 ${props.error ? 'input-error' : 'outline-red-600'}`}
+                className={`block w-full rounded-md bg-white/5 px-3 py-2 lg:text-2xl text-white outline-1
+                 -outline-offset-1  placeholder:text-gray-500 focus:outline-2
+                  focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6
+                   ${props.error ? 'outline-red-600' : 'outline-white/10'}`}
             />
-            {props.error && <span className="text-red-600 text-xs mt-2">{props.error}</span>}
+            {props.errorMessage && <p className="text-red-600 text-xl mt-2">{props.errorMessage}</p>}
         </div>
 
 
--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,18 +1,52 @@
 import InputFormRegistr from "../common/InputFormRegistr.jsx";
+import {useState} from "react";
 
 const Login = () => {
+    const [formaData, setFormaData] = useState({
+        email: "",
+        password: "",
+    })
+    const [errorSate, setErrorState] = useState({
+        bool: false,
+        errorMessage: "",
+    })
+    const handleChange = (type, value) => {
+        setFormaData({ ...formaData, [type]: value });
+    }
+    const existDate = {
+        email: "123@123",
+        password: "12345678",
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(formaData.email === existDate.email && formaData.password === existDate.password) {
+            alert("вы вошли в профиль")
+            setErrorState({
+                bool:false,
+                errorMessage: "",
+            })
+        }
+        else if(formaData.email === existDate.email && formaData.password !== existDate.password){
+            alert("Пароль не верный")
+            setErrorState({
+                    bool: true,
+                    errorMessage: "Пользователя с таким логином и паролем нет"
+                })
+            console.log(errorSate.errorMessage)
+        }
+    }
     return (
 
-        <div className=" px-6 py-12 lg:px-8 w-full ">
+        <div className=" px-6 py-12 lg:px-8 w-full  ">
             <div className="sm:mx-auto w-full sm:w-full ">
-                <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
-                     alt="Your Company" className="mx-auto h-10 w-auto"/>
-                <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">Sign in to your
+                <h2 className="mt-10 text-center text-5xl/9 font-bold tracking-tight text-white">Sign in to your
                     account</h2>
             </div>
 
-            <div className="mt-10 sm:mx-auto w-full sm:w-full sm:max-w-xl">
-                <form action="#" method="POST" className="space-y-6">
+            <div className="mt-20 sm:mx-auto w-full sm:w-full sm:max-w-xl">
+                <form action="#" method="POST"
+                      onSubmit={handleSubmit}
+                      className="space-y-6">
                     <div>
                         <div className="mt-2">
                             <InputFormRegistr
@@ -21,7 +55,8 @@ const Login = () => {
                                 name="email"
                                 label='Email address'
                                 htmlFor="email"
-                                onChange={(e) => handleInputChange('email', e.target.value)}
+                                error={errorSate.bool}
+                                onChange={(e) => handleChange('email', e.target.value)}
                                 autoComplete="email"/>
                         </div>
                     </div>
@@ -29,33 +64,34 @@ const Login = () => {
                     <div>
                         <div className="mt-2">
                             <InputFormRegistr
-                                id="email"
-                                type="email"
-                                name="email"
+                                id="password"
+                                type="password"
+                                name="password"
                                 label='Password'
-                                htmlFor="email"
-                                onChange={(e) => handleInputChange('email', e.target.value)}
-                                autoComplete="email"/>
+                                htmlFor="password"
+                                error={errorSate.bool}
+                                errorMessage={errorSate.errorMessage}
+                                onChange={(e) => handleChange('password', e.target.value)}
+                                autoComplete="password"/>
                         </div>
-                        <div className="text-sm text-right mt-2">
-                            <a href="#" className="font-semibold text-indigo-400 hover:text-indigo-300">Forgot
+                        <div className="text-xl text-right mt-2">
+                            <a href="#" className="font-semibold  text-indigo-400 hover:text-indigo-300">Forgot
                                 password?</a>
                         </div>
                     </div>
 
-                    <div>
+                    <div className="mt-10">
                         <button type="submit"
-                                className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Sign
+                                className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 !text-3xl font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Sign
                             in
                         </button>
                     </div>
                 </form>
 
-                <p className="mt-10 text-center text-sm/6 text-gray-400">
-                    Not a member?
-                    <a href="#" className="font-semibold  text-indigo-400 hover:text-indigo-300">Start a 14 day free
-                        trial</a>
-                </p>
+                <div className="flex justify-between text-center mt-4 text-sm/6 text-gray-400">
+                    <a href="#" className="font-semibold text-2xl text-indigo-400 hover:text-indigo-300">I don't have account</a>
+
+                </div>
             </div>
         </div>
 
